00859d2b02a5a2e3e70d0fea8196d8b6291f8f06
swed1sch <swedisch97@gmail.com>
2020-03-10 16:05:37 +0300

    start configuration

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿
+namespace OnlineLibrary
+{
+    public class Book
+    {
+        public string NameOfBook { get; set; }
+        public string AuthorOfBook { get; set; }
+        public string GenreOfBook { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,8 @@ namespace OnlineLibrary
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddTransient<>
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,13 +24,15 @@ namespace OnlineLibrary
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();                
             }
-
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(name: "default", template: "{ }");
             });
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            
         }
     }
 }
